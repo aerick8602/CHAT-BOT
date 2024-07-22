4827742f9561998159a30af1037d7c838a1a1992
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-07-23 00:39:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-    origin: 'chatbot9685.netlify.app',
+    origin: 'https://chatbot9685.netlify.app',
     credentials: true
 };
 app.use(cors(corsOption));
